5e9f76e8efe12f4550865a3beaba1e2a7694a991
Teena Bee <kbridges@my.waketech.edu>
2021-12-05 22:13:20 -0500

    Board Priniting Functionality Added

---
+++ board.py
@@ -0,0 +1,7 @@
+board_game = [['-', '-', '-'],
+              ['-', '-', '-'],
+              ['-', '-', '-']]
+print('Printing Board')
+for row in board_game:
+    print(row)
+print('Board Printed')
